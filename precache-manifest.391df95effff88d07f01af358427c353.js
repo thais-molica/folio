c955eeb1d86c46db15d6ded1462bac1d6660ecfe
Thais <thaisfaria@cvc.ext.com>
2020-02-28 11:43:35 -0300

    Updates

---
+++ precache-manifest.391df95effff88d07f01af358427c353.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36545fa7b25e0fff73e383e48278f0a4",
+    "url": "/folio/index.html"
+  },
+  {
+    "revision": "85e0c1b2ea0021edb5c0",
+    "url": "/folio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4dec0bed28447bb8be7b",
+    "url": "/folio/static/js/2.f1f8fddd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/folio/static/js/2.f1f8fddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85e0c1b2ea0021edb5c0",
+    "url": "/folio/static/js/main.c7ebf286.chunk.js"
+  },
+  {
+    "revision": "ecd1051e01820af79e45",
+    "url": "/folio/static/js/runtime-main.42aaa839.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/folio/static/media/logo.5d5d9eef.svg"
+  }
+]);
